3d4f5db030433091aeb079bcf3f28582d0c7fc70
dsharshitguptaa7 <harshitguptaa4@gmail.com>
2026-04-14 11:41:07 +0530

    Registration tab is added...

--- script.js
+++ script.js
@@ -93,3 +93,16 @@ function downloadCert() {
     link.click();
     document.body.removeChild(link);
 }
+
+function showTab(tab) {
+    document.querySelectorAll(".tab-content").forEach(el => {
+        el.classList.remove("active");
+    });
+
+    document.querySelectorAll(".tab-btn").forEach(btn => {
+        btn.classList.remove("active");
+    });
+
+    document.getElementById(tab).classList.add("active");
+    event.target.classList.add("active");
+}
