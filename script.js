fb92641dd042bf4dc2c95972a854bfa616c7933d
dsharshitguptaa7 <harshitguptaa4@gmail.com>
2026-04-17 00:31:55 +0530

    Interactive background added

--- script.js
+++ script.js
@@ -195,3 +195,67 @@ window.addEventListener("load", () => {
         });
     });
 });
+
+const canvas = document.getElementById("bgCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particles = [];
+
+for (let i = 0; i < 60; i++) {
+    particles.push({
+        x: Math.random() * canvas.width,
+        y: Math.random() * canvas.height,
+        size: Math.random() * 2,
+        speedX: Math.random() - 0.5,
+        speedY: Math.random() - 0.5
+    });
+}
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    particles.forEach(p => {
+        p.x += p.speedX;
+        p.y += p.speedY;
+
+        if (p.x < 0 || p.x > canvas.width) p.speedX *= -1;
+        if (p.y < 0 || p.y > canvas.height) p.speedY *= -1;
+
+        ctx.fillStyle = "rgba(248, 3, 3, 0.8)";
+        ctx.beginPath();
+        ctx.arc(p.x, p.y, p.size, 0, Math.PI * 2);
+        ctx.fill();
+    });
+
+    requestAnimationFrame(animate);
+}
+
+animate();
+
+/* resize fix */
+window.addEventListener("resize", () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
+
+document.addEventListener("mousemove", e => {
+    document.body.style.setProperty(
+        "--x", e.clientX + "px"
+    );
+});
+
+window.addEventListener("load", () => {
+    const loader = document.getElementById("loader");
+
+    setTimeout(() => {
+        loader.style.opacity = "0";
+        loader.style.transition = "0.5s";
+
+        setTimeout(() => {
+            loader.style.display = "none";
+        }, 500);
+    }, 800); // thoda delay for smooth feel
+});
